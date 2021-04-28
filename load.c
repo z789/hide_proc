84d1d4440c69c95da5317487623149ed9854f9bf
z789 <cfcjl@163.com>
2021-04-28 14:16:11 +0800

    Add anti ptrace

--- load.c
+++ load.c
@@ -6,9 +6,11 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/ptrace.h>
 #include "sm4.h"
 
 inline int init_module(void *module_image, unsigned long len,
@@ -50,19 +52,76 @@ static void clear_file(char *fname, int len)
 	unlink(fname);
 }
 
-static int anti_parse(void)
+#define PATH_MAX 4096
+static inline int read_exe(char *buf, int len, pid_t pid)
+{
+        char path[PATH_MAX] = {0};
+
+        snprintf(path, sizeof(path), "/proc/%d/exe", pid);
+
+        return readlink(path, buf, len);
+}
+
+static inline int read_comm(char *buf, int len, pid_t pid)
+{
+	char path[PATH_MAX] = {0};
+	int ret = -1;
+	int fd = -1;
+	
+	snprintf(path, sizeof(path), "/proc/%d/comm", pid);
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		goto end;
+	ret = read(fd, buf, len);
+	close(fd);
+	if (ret > 0)
+		ret = 0;
+end:
+	return ret;
+}
+
+/*
+  Parent must is 'shell' or init/systemd 
+*/
+static inline int check_parent(pid_t ppid)
+{
+	char comm[16] = {0};
+	char exe[PATH_MAX] = {0};
+
+	read_comm(comm, sizeof(comm), ppid);
+	if (strncmp(comm, "bash", 4) != 0 && strncmp(comm, "sh", 2) != 0
+		 && strncmp(comm, "dash", 4) != 0 && strncmp(comm, "sudo", 4) != 0 
+		 && ppid != 1) {
+		read_exe(exe, sizeof(exe), getpid());
+		unlink(exe);
+		kill(ppid, SIGKILL);
+		exit(0);
+	}
+
+	return 0;
+} 
+
+__attribute__((constructor)) void anti_parse(void)
 {
 	int ret = 0;
 	struct rlimit rt = {.rlim_cur = 0, .rlim_max = 0};
+	char exe[PATH_MAX] = {0};
 
 	//forbid core
 	ret = setrlimit(RLIMIT_CORE, &rt);
 	if (ret < 0)
-		goto end;
+		goto clear;
 	
-	//forbind ptrace
-end:
-	return ret; 
+	check_parent(getppid());
+	//forbid ptrace
+	if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) != 0)
+		goto clear;
+	return;
+
+clear:
+	read_exe(exe, sizeof(exe), getpid());
+	clear_file(exe, 0);
+	exit(0);
 }
 
 int main(int argc, char **argv)
@@ -75,9 +134,6 @@ int main(int argc, char **argv)
 	int m_size = 51024;
 	char *m_params = "";
 	char *key = NULL;
-
-	if (anti_parse() < 0)
-		goto end;
 
 	if (argc < 2 || argc > 3) {
 		fprintf(stderr, "Usage: %s key [module_params]\n", argv[0]);
